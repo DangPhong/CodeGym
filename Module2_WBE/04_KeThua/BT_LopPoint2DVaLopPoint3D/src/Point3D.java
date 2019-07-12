0ef2802b8f90fd3b9a151539e6c44bbef23ef889
Dang Phong <dangphong.92.tt@gmail.com>
2019-07-12 12:17:59 +0700

    add files

---
+++ Module2_WBE/04_KeThua/BT_LopPoint2DVaLopPoint3D/src/Point3D.java
@@ -0,0 +1,26 @@
+public class Point3D extends Point2D {
+    private float z = 0.0f;
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " và tọa độ Y là: " + getZ();
+    }
+
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D(6, 5, 3);
+        System.out.println(point3D);
+    }
+}
