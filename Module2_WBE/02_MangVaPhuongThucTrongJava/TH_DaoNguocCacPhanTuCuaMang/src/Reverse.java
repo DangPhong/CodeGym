c417f45e0f212f1cd251635c01bad2e06a9c97c7
Dang Phong <dangphong.92.tt@gmail.com>
2019-07-16 00:31:35 +0700

    add files

--- Module2_WBE/02_MangVaPhuongThucTrongJava/TH_DaoNguocCacPhanTuCuaMang/src/Reverse.java
+++ Module2_WBE/02_MangVaPhuongThucTrongJava/TH_DaoNguocCacPhanTuCuaMang/src/Reverse.java
@@ -13,10 +13,6 @@ public class Reverse {
             arr[i] = arr[length - 1 - i];
             arr[length - 1 - i] = temp;
         }
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.print(arr[i] + "\t");
-//        }
-//        return arr;
     }
 
     public static void main(String[] args) {
